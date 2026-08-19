8725b34ab652003bbadd3678e9933cc9fd1432c7
Jendry Murillo <jendry.murillo@ulatina.net>
2026-08-19 12:23:19 -0600

    fix: adapta servidor para despliegue en Render

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
